357454a0d21e5592804be531ce6b10712ce172e1
panmari <smmuzi@gmail.com>
2025-08-01 15:18:00 +0200

    Add example test for usage together with S2 libraries.

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module github.com/google/go-units
 
-go 1.21
+go 1.21.0
 
-require github.com/google/go-cmp v0.7.0
+toolchain go1.22.2
+
+require (
+	github.com/golang/geo v0.0.0-20250731010204-92ad70d864ba
+	github.com/google/go-cmp v0.7.0
+)
--- unit/example_test.go
+++ unit/example_test.go
@@ -3,6 +3,8 @@ package unit_test
 import (
 	"fmt"
 
+	"github.com/golang/geo/earth"
+	"github.com/golang/geo/s2"
 	"github.com/google/go-units/unit"
 )
 
@@ -26,4 +28,15 @@ func ExampleLength() {
 	// I would walk 500 miles, but also 804.7 kilometers.
 	// Altitude: 8848.65 m
 	// And after I walk 500 miles more, that's a total of 869.0 nautical miles.
+}
+
+func ExampleLength_fromS2() {
+	sanFrancisco := s2.LatLngFromDegrees(37.7749, -122.4194)
+	zurich := s2.LatLngFromDegrees(47.3769, 8.5417)
+
+	d := earth.LengthFromLatLngs(sanFrancisco, zurich)
+
+	fmt.Printf("The distance from San Francisco to Zurich is %.2f km or %.2f miles.\n", d.Kilometers(), d.Miles())
+	// Output:
+	// The distance from San Francisco to Zurich is 9370.33 km or 5822.45 miles.
 }
